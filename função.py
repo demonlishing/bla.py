d3d0ce3ca392f26ffd678c0fcb83c7653dbea55f
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-06-10 09:00:30 -0300

    Update função.py

--- função.py
+++ função.py
@@ -8,6 +8,7 @@ def apresentarpalavra(letras, palavra):
         for p in range(0 , len(palavra)):      
             #print(palavra[p])
             if letras[x]== palavra[p]:
+                npalavra = npalavra[0:p*2] + palavra[p] + "_ " + npalavra[p*2: ]
                 print(letras[x])
     return npalavra
 
